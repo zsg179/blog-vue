0ac8ca91c88682cee9354577aeb0d5719a9bc687
zsg179 <18977330642@163.com>
2020-07-21 18:21:52 +0800

    新建了首页，修改了资源访问路径

--- myblog/src/main.js
+++ myblog/src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
+//axios.defaults.baseURL = "http://pekka.icu/blogvue/"
+Vue.prototype.$axios = axios
+Vue.use(Element)
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
 
 /* eslint-disable no-new */
 new Vue({
--- myblog/src/router/index.js
+++ myblog/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +7,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: () => import('@/views/index'),
+      meta:{
+        title: '朱树广的个人博客'
+      }
     }
   ]
 })
